1c6212b8af49104c8407a4b7533fe8f5ce582f94
nancy2008 <dany_gpe2008@hotmail.com>
2016-09-03 12:29:03 -0500

    Add files via upload

---
+++ ejercicios Nancy.py
@@ -0,0 +1,25 @@
+ejercicio 1
+introducir un numero por teclado y decir si es par o impar.
+h=input('introduzca un numero;') 
+if h=&2==0:
+   print("este numero es par")
+   else:
+   print("este numero es impar")
+
+
+
+ejercicio
+haz un programa que pida tres numeros y los ordene de mayor a menor
+numero01=input()
+numero02=input()
+numero03=input()
+if(numero01>numero02>numero03):
+   print numero01,"y",numero02,"y",numero03
+elif(numero01>numero03>>numero02):
+   printnumero01,"y",numero03,"y",numero02
+elif(numero02>numero01>numero03):
+   print numero02,"y",numero01,"y",numero03
+elif(numero02>numero03>numero01):
+   print numero02,"y",numero02;"y",numero01
+elif(numero03>numero01>numero02):
+   print numero03,"y",numero01,"y",numero02 
